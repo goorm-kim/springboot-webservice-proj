68b0a465bcbd1a88043dd51dc958f45a4e48d6fc
goorm-kim <dnluvletter@gmail.com>
2023-01-30 15:47:50 +0900

    fixed untracked files

---
+++ src/main/java/com/dnluvletter/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.dnluvletter.springboot.web.dto;
+
+import com.dnluvletter.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+
+}
